c846fb0f953cc15b1ac83a5637cca94e8edfebcd
dav4 <diegovelasquezavila@gmail.com>
2024-06-10 12:50:05 -0600

    delete changes of cors

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/index.js';
-import allowCors from './allowCors.js';
 
 const app = express();
 
 app.use(cors());
 
-app.use(allowCors);
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+  
 app.use('/api', routes);
 
 export default app;
