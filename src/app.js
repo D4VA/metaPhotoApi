804fdde3387c45cb2997beac1609069933c710d2
dav4 <diegovelasquezavila@gmail.com>
2024-06-10 00:55:01 -0600

    update cors allows

--- src/app.js
+++ src/app.js
@@ -4,7 +4,12 @@ import routes from './routes/index.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // Cambia esto por el puerto de tu aplicación de frontend
+  methods: 'GET,POST,PUT,DELETE', // Ajusta los métodos permitidos según tu aplicación
+  allowedHeaders: 'Content-Type,Authorization', // Ajusta los encabezados permitidos según tu aplicación
+}));
+
 app.use(express.json());
 app.use('/api', routes);
 
