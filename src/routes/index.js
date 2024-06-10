ce3814874c27211a70e427c9bd965e149c9f79cb
dav4 <diegovelasquezavila@gmail.com>
2024-06-10 00:50:37 -0600

    update cors to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { searchTitleData, getCombinedDataById } from '../controllers/combinedController.js';
+import allowCors from './allowCors.js';
 
 const router = express.Router();
 
-router.get('/photos', searchTitleData);
-router.get('/photos/:id', getCombinedDataById);
+router.get('/photos', allowCors(searchTitleData));
+router.get('/photos/:id', allowCors(getCombinedDataById));
 
 export default router;
