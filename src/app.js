3d15f764951b818c70eedc7d5c080c883efcbc15
dav4 <diegovelasquezavila@gmail.com>
2024-06-10 01:58:18 -0600

    update config cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/index.js';
+import allowCors from './allowCors.js';
 
 const app = express();
 
-// Configuración de CORS para permitir todas las solicitudes
 app.use(cors());
+
+app.use(allowCors);
 
 app.use(express.json());
 app.use('/api', routes);
